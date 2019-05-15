015a894660a96d21f183d95edcf99e68737c3793
Tess Neau <tessneau@gmail.com>
2019-05-15 14:35:13 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    words = self.split(' ')
+    words.count do |word|
+      word.sentence? || word.question? || word.exclamation?
+    end
   end
 end
